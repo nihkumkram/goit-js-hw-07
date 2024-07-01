40d6967e13227089bf08fdbc2623a41274b9a6f5
nihkumkram <koteya754@gmail.com>
2024-07-01 03:28:21 +0200

    4

--- src/js/task-3.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", () => {
-  const trimmedValue = nameInput.value.trim();
-  nameOutput.textContent = trimmedValue !== "" ? trimmedValue : "Anonymous";
-});
